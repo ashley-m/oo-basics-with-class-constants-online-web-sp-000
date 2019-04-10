4bd99608768f4442b64f1b51980cbb4b104c939a
Ashley Miller <asm2fe@virginia.edu>
2019-04-10 00:24:21 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -2,8 +2,15 @@ class Book
   attr_accessor :author, :page_count, :genre
   attr_reader :title
 
+  GENRES = []
+  
   def initialize(title)
     @title = title
+  end
+
+  def @genre=(g)
+    @genre = g
+    GENRES << g
   end
 
   def turn_page
